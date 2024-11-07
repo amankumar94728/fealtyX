eda3b815f01bb2dbbba1bec064c5644f2b9aa001
AMAN KUMAR <amankumar@AMANs-MacBook-Air.local>
2024-11-07 20:54:33 +0530

    Added Test Route

--- main.go
+++ main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
-    "github.com/amankumar94728/fealtyx-student-api/internal/api"
-    "github.com/amankumar94728/fealtyx-student-api/internal/storage"
+	"github.com/amankumar94728/fealtyx-student-api/internal/api"
+	"github.com/amankumar94728/fealtyx-student-api/internal/storage"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    store := storage.NewStorage()
-    apiHandler := api.NewAPI(store)
+	store := storage.NewStorage()
+	apiHandler := api.NewAPI(store)
 
-    r := mux.NewRouter()
-    r.HandleFunc("/students", apiHandler.CreateStudent).Methods("POST")
-    r.HandleFunc("/students", apiHandler.GetAllStudents).Methods("GET")
-    r.HandleFunc("/students/{id}", apiHandler.GetStudentByID).Methods("GET")
-    r.HandleFunc("/students/{id}", apiHandler.UpdateStudent).Methods("PUT")
-    r.HandleFunc("/students/{id}", apiHandler.DeleteStudent).Methods("DELETE")
-    r.HandleFunc("/students/{id}/summary", apiHandler.GenerateStudentSummary).Methods("GET")
+	r := mux.NewRouter()
 
-    log.Println("Server starting on :8080")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is running!"))
+	}).Methods("GET")
+
+	r.HandleFunc("/students", apiHandler.CreateStudent).Methods("POST")
+	r.HandleFunc("/students", apiHandler.GetAllStudents).Methods("GET")
+	r.HandleFunc("/students/{id}", apiHandler.GetStudentByID).Methods("GET")
+	r.HandleFunc("/students/{id}", apiHandler.UpdateStudent).Methods("PUT")
+	r.HandleFunc("/students/{id}", apiHandler.DeleteStudent).Methods("DELETE")
+	r.HandleFunc("/students/{id}/summary", apiHandler.GenerateStudentSummary).Methods("GET")
+
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
